698355ceb07016f7848e00f733e534b0c1bdd67f
Benyamin Limanto <blbenyamin9@gmail.com>
2021-11-23 11:55:56 +0700

    Adding Primary key Trait

---
+++ app/Trait/HasCompositePrimaryKey.php
@@ -0,0 +1,49 @@
+<?php
+// @see https://stackoverflow.com/a/37076437/4906348
+// @see https://github.com/laravel/framework/issues/5355#issuecomment-161376267
+namespace App\Trait;
+
+/**
+ *
+ */
+trait HasCompositePrimaryKey
+{
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery($query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
+}
